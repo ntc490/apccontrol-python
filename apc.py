4b95643a4f71905245ebb485dbb3c7fe9bcf03c6
Nathan Crapo <nathan_crapo@yahoo.com>
2021-07-14 22:25:54 -0600

    Update usage after a little more thought

--- apc.py
+++ apc.py
@@ -2,15 +2,24 @@
 """apc.py - Control APC network power strip
 
 Usage:
-  apc.py (on [<port>] | off [<port>] | reset [<port>] | list)
-  apc.py list-aliases
-  apc.py set-alias <name> <num>
-  apc.py rm-alias <name>
-  apc.py set-host <hostname>
+  apc.py [options] (on [<port>] | off [<port>] | reset [<port>] | list)
+  apc.py [options] set-alias <name> <num>
+  apc.py [options] rm-alias <name>
+  apc.py [options] set-host <hostname>
   apc.py --help
 
+Commands:
+  on                     Turn port on [defaults to last port if empty]
+  off                    Turn port off [default to last port if empty]
+  reset                  Reset port [default to last port if empty]
+  list                   List all ports, their aliases, and their status
+  set-alias              Set an alias for a port number
+  rm-alias               Remove alias for a port
+  set-host               Set host of APC device via IP address or hostname
+  --help                 Print this usage screen
+
 Options:
-  --help                              Print this usage screen
+  --config <filename>    Point to custom config file [default: ~/.config/apc/config]
 
 """
 import docopt
