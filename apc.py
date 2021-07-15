4419ecf0205a3a3dbb9f0507c256647cc8e5c073
Nathan Crapo <nathan_crapo@yahoo.com>
2021-07-14 22:39:32 -0600

    Application outline functions

--- apc.py
+++ apc.py
@@ -22,12 +22,58 @@ Options:
   --config <filename>    Point to custom config file [default: ~/.config/apc/config]
 
 """
+import sys
 import docopt
 
 
 def main():
     args = docopt.docopt(__doc__)
-    print(args)
+    print(args) # TODO: remove this line - initial debug ONLY
+    config = ConfigFile(args['--config'])
+    error = run_command(args, config)
+    sys.exit(error)
+    
+
+# --------------- Classes ---------------
+
+class ConfigFile(object):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read():
+        pass
+
+    def port_name(port_num):
+        pass
+
+    def port_num(port_name):
+        pass
+
+# --------------- Command Handlers ---------------
+
+def on_command(args, config):
+    pass
+
+def off_command(args, config):
+    pass
+
+def reset_command(args, config):
+    pass
+
+def list_command(args, config):
+    pass
+
+def set_alias_command(args, config):
+    pass
+
+def rm_alias_command(args, config):
+    pass
+
+def set_host_command(args, config):
+    pass
+
+def run_command(args, config):
+    pass
 
 if __name__ == "__main__":
     main()
